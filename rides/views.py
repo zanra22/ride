1e6dd0a9bcc937ff3c5a0baa7bf88893d9de064b
Arnaz De Jesus <arnazdj@gmail.com>
2024-07-12 09:11:20 +0800

    Changed the if statement in get_serializer_class method for better readability

--- rides/views.py
+++ rides/views.py
@@ -1,36 +1,28 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-
 from .models import Ride, RideEvent
 from .serializers import RideSerializer, CreateRideSerializer
 from .filters import RideFilter
 from .pagination import CustomPageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
-
 from rest_framework.permissions import IsAuthenticated
 from accounts.permissions import IsAdminUser
 from django.utils import timezone
 from datetime import datetime
 from datetime import timedelta
 from django.db.models import Q, FilteredRelation, Prefetch
-
-
 # Create your views here.
-
-
 class RideViewSet(viewsets.ModelViewSet):
-    # serializer_class = RideSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = RideFilter
     permission_classes = [IsAuthenticated, IsAdminUser]
     pagination_class = CustomPageNumberPagination
 
-
     def get_serializer_class(self):
-        if self.action == 'list' or self.action == 'retrieve':
+        if self.action in ['list', 'retrieve']:
             return RideSerializer
-        elif self.action == 'create' or self.action == 'update':
+        elif self.action in ['create', 'update']:
             return CreateRideSerializer
         return RideSerializer
 
@@ -56,7 +48,6 @@ class RideViewSet(viewsets.ModelViewSet):
                      queryset=RideEvent.objects.filter(created_at__gte=current_time - timedelta(hours=24)))
         )
         return queryset
-
     #Customided list method to include total_count in the response.
     def list(self, request, *args, **kwargs):
         try:
